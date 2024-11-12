276c9e3af350aeed51ab381998ebcf78e89b90c7
pro-minhaj <parsonal322532@gmail.com>
2024-11-13 03:56:54 +0600

    Optimize change not status component code and logic fix

--- app/(dashboard)/components/change-bot-status.jsx
+++ app/(dashboard)/components/change-bot-status.jsx
@@ -3,57 +3,36 @@ import { changeBotStatus } from "@/app/actions/bot";
 import SubmitButton from "@/components/globals/Buttons/submit-button";
 import { useSidebar } from "@/components/ui/sidebar";
 import { CirclePause, CirclePlay } from "lucide-react";
+import { startTransition } from "react";
 import { toast } from "sonner";
 
 const ChangeBotStatus = ({ isBotAction }) => {
     const { state } = useSidebar();
 
-    const handleBotStatus = async () => {
-        try {
-            const botStatus = await changeBotStatus(isBotAction);
-            if (botStatus) {
-                toast.success("Bot Started Success")
+    const handleBotStatus = () => {
+        startTransition(async () => {
+            try {
+                const botStatus = await changeBotStatus(isBotAction);
+                toast.success(botStatus ? "Bot Started Successfully" : "Bot Stopped Successfully");
+            } catch (error) {
+                toast.error(`Error updating bot status: ${error.message}`);
             }
-            else {
-                toast.success("Bot Stop Success")
-            }
-        } catch (error) {
-            toast.error('Error updating bot status:', error.message);
-        }
-    }
+        });
+    };
 
+    const icon = isBotAction ? <CirclePause className="!w-5 !h-5" /> : <CirclePlay className="!w-5 !h-5" />;
+    const buttonText = isBotAction ? "Stop Bot" : "Start Bot";
+    const buttonVariant = isBotAction ? "destructive" : "default";
 
     return (
         <form action={handleBotStatus}>
-            {
-                isBotAction ? (
-                    <SubmitButton className="font-semibold">
-                        {
-                            state === "collapsed" ? (
-                                <CirclePlay className="!w-5 !h-5" />
-                            ) : (
-                                <>
-                                    <CirclePlay className="!w-5 !h-5" />
-                                    Start Bot
-                                </>
-                            )
-                        }
-                    </SubmitButton>
-                ) : (
-                    <SubmitButton variant="destructive" className="font-semibold">
-                        {
-                            state === "collapsed" ? (
-                                <CirclePause className="!w-5 !h-5" />
-                            ) : (
-                                <>
-                                    <CirclePause className="!w-5 !h-5" />
-                                    Stop Bot
-                                </>
-                            )
-                        }
-                    </SubmitButton>
-                )
-            }
+            <SubmitButton
+                variant={buttonVariant}
+                className="font-semibold"
+            >
+                {icon}
+                {state !== "collapsed" && buttonText}
+            </SubmitButton>
         </form>
     );
 };
